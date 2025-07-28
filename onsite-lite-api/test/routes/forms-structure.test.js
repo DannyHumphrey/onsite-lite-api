8f14dc96e139d0f6c6510cf652c477b679af0e96
DannyHumphrey <175.danny@gmail.com>
2025-07-28 19:56:51 +0100

    Implement forms structure endpoint with roles

--- onsite-lite-api/test/routes/forms-structure.test.js
+++ onsite-lite-api/test/routes/forms-structure.test.js
@@ -12,15 +12,51 @@ test('structure requires auth', async (t) => {
   assert.equal(res.statusCode, 401)
 })
 
-test('structure returns ok with jwt', async (t) => {
+test('structure rejects invalid token', async (t) => {
   const app = await build(t)
-  const token = app.jwt.sign({ id: 1 })
   const res = await app.inject({
     url: path,
-    headers: {
-      authorization: `Bearer ${token}`
-    }
+    headers: { authorization: 'Bearer invalid' }
   })
+  assert.equal(res.statusCode, 401)
+})
+
+test('structure returns schemas for roles', async (t) => {
+  const app = await build(t)
+
+  // stub database query
+  app.db.query = async () => {
+    return {
+      recordset: [
+        {
+          formTypeId: 1,
+          name: 'Magenta EICR (V2)',
+          schemaJson: '{"foo": "bar"}',
+          version: 3
+        }
+      ]
+    }
+  }
+
+  const token = app.jwt.sign({
+    id: 1,
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/role': [
+      'Magenta EICR (V2)'
+    ]
+  })
+
+  const res = await app.inject({
+    url: path,
+    headers: { authorization: `Bearer ${token}` }
+  })
+
   assert.equal(res.statusCode, 200)
-  assert.deepStrictEqual(JSON.parse(res.payload), { message: 'OK' })
+  assert.deepStrictEqual(JSON.parse(res.payload), [
+    {
+      formTypeId: 1,
+      name: 'Magenta EICR (V2)',
+      schema: { foo: 'bar' },
+      version: 3
+    }
+  ])
 })
