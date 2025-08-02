e17d8a222cde846c4e510194c86762db75bc1c43
daniel humphrey <>
2025-08-02 12:16:14 +0100

    update init

--- onsite-lite-api/routes/api/forms.js
+++ onsite-lite-api/routes/api/forms.js
@@ -3,7 +3,7 @@
 const formService = require('../../services/formService')
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/forms/structure', { preValidation: [fastify.authenticate] }, async function (request, reply) {
+  fastify.get('/FormTemplates', { preValidation: [fastify.authenticate] }, async function (request, reply) {
     const claim = request.user['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
     const roles = Array.isArray(claim) ? claim : claim ? [claim] : []
     const schemas = await formService.getLatestSchemasByRoles(roles, fastify.db)
--- onsite-lite-api/scripts/init-db.js
+++ onsite-lite-api/scripts/init-db.js
@@ -3,13 +3,16 @@
 const { init, query, getPool } = require('../db')
 
 async function run () {
+
   const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    server: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    user: process.env.DB_USER ?? 'SA',
+    password: process.env.DB_PASS ?? '123456a@',
+    server: process.env.DB_HOST ?? 'localhost',
+    database: process.env.DB_NAME ?? 'OnsiteLite',
     options: { encrypt: false, trustServerCertificate: true }
   }
+
+  console.debug(config)
 
   await init(config)
 
